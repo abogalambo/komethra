9dd91aed003a24f3b0c5f501921c1c50dcf60c76
thedecimal <yaser.al3shry@gmail.com>
2017-07-30 21:47:19 +0200

    initial commit

---
+++ src/components/slides/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class Slides extends Component {
+  render() {
+    // You can use them as regular CSS styles
+    return <div className="slides" />;
+  }
+}
