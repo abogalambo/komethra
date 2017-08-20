7e0db682b67f77f51e907eb34874d7f5d68935bc
fufukha <wafakhalid29@gmail.com>
2017-08-20 17:43:12 -0400

    Style changes * Add reset.css * Add home page stylesheet * Add general styles

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import './index.css';
+import './index.css';
 
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <h2>Home</h2>
-        <Link to="/decks/1/cards/1">Start learning now!</Link>
+      <div className="home">
+        <div className="home__container vert-center">
+          <div className="home__content">
+            <header className="home__header">
+              <h1 className="header__title">Komethra</h1>
+              <h2 className="header__title--ar">كمثرى</h2>
+            </header>
+            <Link className="btn btn--primary" to="/decks/1/cards/1">
+              Learn now!
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }
