167d84d5570e5a7254b0d0c0a5cd17f401cd440e
fufukha <wafakhalid29@gmail.com>
2017-11-06 17:06:40 -0500

    Fix boxShadow property name

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -14,7 +14,7 @@ export default class Card extends Component {
             className={classNames('card', { flipped: isFlipped })}
             style={
               current && {
-                'box-shadow':
+                boxShadow:
                   '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'
               }
             }
